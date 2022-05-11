7fc29e83dbdf568639bc71e36e11d55eac9569a4
addKiwi <skrypak1997@gmail.com>
2022-05-11 17:27:28 +0300

    fixed card overflow

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,25 @@
-export const API_URL = 'https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=5';
+export const API_URL = 'https://frontend-test-assignment-api.abz.agency/api/v1';
 
 export function request(query: string, method = {}) {
   return fetch(query, method)
     .then(response => response.json());
 }
 
-export const getUsers = (query=API_URL) => request(query);
+export const getUsers = (query=`${API_URL}/users?page=1&count=5`) => request(query);
+
+export const getPositions = () => request(`${API_URL}/positions`);
+
+export const getToken = () => request(`${API_URL}/token`);
+
+export const addUser = (formData: FormData, token: string) => (
+  request('/comments', {
+    method: 'POST',
+    headers: {
+      'Token': token,
+    },
+    body: formData,
+  })
+);
 
 // export const getUserById = (userId: number): Promise<User> => request(`/users/${userId}`);
 
